0de7121e6f7fdeffd1fd785e47409436675fcdc7
Abraham Lipa Calabilla <abrlipac@upt.pe>
2021-06-22 18:38:20 -0500

    first commit

---
+++ app/src/main/java/tech/abralica/reconocimientovoz/MainActivity.java
@@ -0,0 +1,96 @@
+package tech.abralica.reconocimientovoz;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button bStart, bStop;
+    private SpeechRecognizer speechRecognizer;
+    private Intent intentRecognizer;
+    private TextView tvMensaje;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        requestPermissions(new String[]{Manifest.permission.RECORD_AUDIO}, PackageManager.PERMISSION_GRANTED);
+
+        bStart = findViewById(R.id.am_b_iniciar);
+        bStop = findViewById(R.id.am_b_detener);
+        tvMensaje = findViewById(R.id.am_tv_mensaje);
+
+        intentRecognizer = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intentRecognizer.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+
+        speechRecognizer = SpeechRecognizer.createSpeechRecognizer(this);
+        speechRecognizer.setRecognitionListener(new RecognitionListener() {
+            @Override
+            public void onReadyForSpeech(Bundle params) {
+
+            }
+
+            @Override
+            public void onBeginningOfSpeech() {
+
+            }
+
+            @Override
+            public void onRmsChanged(float rmsdB) {
+
+            }
+
+            @Override
+            public void onBufferReceived(byte[] buffer) {
+
+            }
+
+            @Override
+            public void onEndOfSpeech() {
+
+            }
+
+            @Override
+            public void onError(int error) {
+
+            }
+
+            @Override
+            public void onResults(Bundle results) {
+                ArrayList<String> matches = results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+                String string = "";
+
+                if (matches != null) {
+                    string = matches.get(0);
+                    tvMensaje.setText(string);
+                }
+            }
+
+            @Override
+            public void onPartialResults(Bundle partialResults) {
+
+            }
+
+            @Override
+            public void onEvent(int eventType, Bundle params) {
+
+            }
+
+        });
+
+        bStart.setOnClickListener(v -> speechRecognizer.startListening(intentRecognizer));
+        bStop.setOnClickListener(v -> speechRecognizer.stopListening());
+    }
+}
